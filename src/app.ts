1bc18695f7fb5663fd90937ea0d5231fb9ca6692
Rafael Fernandes Lopes <rafael.fernandes@gmail.com>
2023-07-17 00:23:59 -0300

    feat: add helmet to increase Express security
    helmet helps secure Express apps by setting HTTP response headers

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import fs from "fs";
 import path from "path";
 import express from "express";
 import cors from "cors";
+import helmet from "helmet";
 import createError from "http-errors";
 import cookieParser from "cookie-parser";
 
@@ -31,6 +32,7 @@ const swaggerDocument = yaml.parse(file);
 app.use(httpLogger);
 
 app.use(cors());
+app.use(helmet());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
